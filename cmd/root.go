8a20c8f40b4229d83da87ea5ec1b845d6d8c5fc1
pyr33x <pyrexrj@gmail.com>
2025-09-06 22:21:31 +0330

    chore: remove unused comment

--- cmd/root.go
+++ cmd/root.go
@@ -6,7 +6,6 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "benchmq",
 	Short: "BenchMQ is a simple, fast, and lightweight CLI to benchmark your MQTT broker with ease.",
