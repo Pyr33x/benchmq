753c627fb05fd65f0fa0819a2068ede308f842c8
pyr33x <pyrexrj@gmail.com>
2025-09-06 22:45:12 +0330

    feat(mqtt): add mqtt client adapter

--- go.mod
+++ go.mod
@@ -3,11 +3,15 @@ module github.com/pyr33x/benchmq
 go 1.24.1
 
 require (
+	github.com/eclipse/paho.mqtt.golang v1.5.0
 	github.com/spf13/cobra v1.10.1
 	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
+	github.com/gorilla/websocket v1.5.3 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.9 // indirect
+	golang.org/x/net v0.27.0 // indirect
+	golang.org/x/sync v0.7.0 // indirect
 )
---
+++ internal/mqtt/mqtt.go
@@ -0,0 +1,45 @@
+package mqtt
+
+import (
+	"fmt"
+	"sync"
+	"time"
+
+	mq "github.com/eclipse/paho.mqtt.golang"
+	"github.com/pyr33x/benchmq/cmd"
+	"github.com/pyr33x/benchmq/pkg/er"
+)
+
+type Adapter struct {
+	client mq.Client
+	wg     sync.WaitGroup
+}
+
+func NewClient(cfg cmd.Config) *Adapter {
+	// Initialize MQTT client options
+	opts := mq.NewClientOptions()
+
+	opts.AddBroker(fmt.Sprintf("tcp://%s:%d", cfg.Server.Host, cfg.Server.Port))
+	opts.SetClientID(cfg.Client.ClientID)
+	opts.SetKeepAlive(time.Duration(cfg.Client.KeepAlive) * time.Second)
+	opts.SetCleanSession(cfg.Client.CleanSession)
+	opts.SetUsername(cfg.Client.Username)
+	opts.SetPassword(cfg.Client.Password)
+	opts.SetProtocolVersion(4) // Default set to MQTT 3.1.1
+
+	// Create a new MQTT client instance
+	client := mq.NewClient(opts)
+
+	// Return the initialized MQTT adapter
+	return &Adapter{client: client}
+}
+
+func (a *Adapter) Connect() error {
+	if token := a.client.Connect(); token.Wait() && token.Error() != nil {
+		return &er.Error{
+			Context: "MQTT Adapter",
+			Message: er.ErrMqttConnectionFailed,
+		}
+	}
+	return nil
+}
