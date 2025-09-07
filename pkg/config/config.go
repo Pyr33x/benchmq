5cbf0c0744ddcdfd73b48373c4e588a03d83f8c9
pyr33x <pyrexrj@gmail.com>
2025-09-07 12:46:36 +0330

    feat(pkg): add validation method over config struct

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,9 +1,9 @@
 package config
 
 import (
-	"log"
 	"os"
 
+	"github.com/pyr33x/benchmq/pkg/er"
 	"gopkg.in/yaml.v3"
 )
 
@@ -30,17 +30,62 @@ type client struct {
 }
 
 // InitializeCfg reads the config file and returns a pointer to the Config struct
-func InitializeCfg() *Config {
+func InitializeCfg() (*Config, error) {
 	rawCfg, err := os.ReadFile("config.yml")
 	if err != nil {
-		log.Println("Failed to read the configuration file")
+		return nil, &er.Error{
+			Package: "Config",
+			Func:    "InitializeCfg",
+			Message: er.ErrConfigReadFailed,
+		}
 	}
 
 	var cfg Config
 	err = yaml.Unmarshal(rawCfg, &cfg)
 	if err != nil {
-		log.Println("Failed to unmarshal the yaml configuration file")
+		return nil, &er.Error{
+			Package: "Config",
+			Func:    "InitializeCfg",
+			Message: er.ErrUnmarshalFailed,
+		}
 	}
 
-	return &cfg
+	cfg.SetDefaults()
+	if err := cfg.Validate(); err != nil {
+		return nil, err
+	}
+
+	return &cfg, nil
+}
+
+// Validate does the validation over server configuration
+func (c *Config) Validate() error {
+	if c.Server.Host == "" {
+		return &er.Error{
+			Package: "Config",
+			Func:    "Validate",
+			Message: er.ErrEmptyServerHost,
+		}
+	}
+	if c.Server.Port == 0 || c.Server.Port > 65535 {
+		return &er.Error{
+			Package: "Config",
+			Func:    "Validate",
+			Message: er.ErrInvalidServerPort,
+		}
+	}
+	return nil
+}
+
+// SetDefaults sets the default values to fields when values are not acceptable
+func (c *Config) SetDefaults() {
+	if c.Server.Host == "" {
+		c.Server.Host = "localhost"
+	}
+	if c.Server.Port == 0 {
+		c.Server.Port = 1883
+	}
+	if c.Client.KeepAlive == 0 {
+		c.Client.KeepAlive = 60
+	}
 }
