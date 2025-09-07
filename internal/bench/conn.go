27151aab743a683f6a08892a7d4fcb9569178c1e
pyr33x <pyrexrj@gmail.com>
2025-09-07 14:55:50 +0330

    fix(bench): copy cfg to avoid races

--- internal/bench/conn.go
+++ internal/bench/conn.go
@@ -10,19 +10,27 @@ import (
 
 func (b *Bench) RunConnections() {
 	now := time.Now()
+
 	for i := 0; i < b.clients; i++ {
 		b.wg.Add(1)
 		go func(id int) {
 			defer b.wg.Done()
-			b.cfg.Client.ClientID = fmt.Sprintf("benchmq-client-%d", id)
-			client := mqtt.NewClient(b.cfg)
+
+			cfg := *b.cfg
+			cfg.Client.ClientID = fmt.Sprintf("benchmq-client-%d", id)
+			client := mqtt.NewClient(&cfg)
 			defer client.Disconnect()
-			log.Printf("Client: %s ― Connected", b.cfg.Client.ClientID)
+
+			log.Printf("Client: %s ― Connecting", cfg.Client.ClientID)
 			if err := client.Connect(); err != nil {
-				log.Printf("client: %d failed: %v", id, err)
+				log.Printf("Client: %d ― Failed: %v", id, err)
+				return
 			}
+			log.Printf("Client: %s ― Connected", cfg.Client.ClientID)
+
 		}(i)
 	}
+
 	b.wg.Wait()
 	log.Println(time.Since(now))
 }
